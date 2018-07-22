911e06ad95d3f8cb122468d95b22247772011ffd
jamesdamillington@gmail.com <jamesdamillington@gmail.com>
2018-07-22 12:21:11 +0100

    code for ggplots of land cover maps

--- 7_outputMovies.R
+++ 7_outputMovies.R
@@ -23,6 +23,8 @@ library(rasterVis)
 library(gridExtra)
 library(RColorBrewer)
 library(animation)
+library(sf)
+library(ggpubr)
 
 outputRaster <- function(data, variable){
   
@@ -35,18 +37,23 @@ outputRaster <- function(data, variable){
   
 }
 
-#this directory should exist and contain the output files and region file from the run being examined
-run_name <- "test"
-plot_yrs <- seq(2000, 2005, 1)
-scenario <- "Testing-0-0"
 
+plot_yrs <- seq(2000, 2005, 1)
+
+
+#this directory should exist and contain the output files and region file from the run being examined
+scenario <- "Testing"
+runID <- "0-0"
+
+
+#First, Raster maps
 mps <- list()
 lus <- list()
 
 for(i in seq_along(plot_yrs)){
   
   #Load model output data
-  output <- read_csv(paste0("Data/",run_name,"/",scenario,"-Cell-",plot_yrs[i],".csv"))
+  output <- read_csv(paste0("Data/",scenario,"/",runID,"/",scenario,"-",runID,"-Cell-",plot_yrs[i],".csv"))
   
   LU <- outputRaster(output, "LandUseIndex")
   Agri <- outputRaster(output, "Capital:Agriculture")
@@ -83,7 +90,7 @@ for(i in seq_along(plot_yrs)){
     LUcols <- c(LUcols, 'forestgreen')}
   if(5 %in% uLU) { 
     labs <- c(labs, "OAgri") 
-    LUcols <- c(LUcols, 'gray')}
+    LUcols <- c(LUcols, 'darkcyan')}
   if(6 %in% uLU) { 
     labs <- c(labs, "Other") 
     LUcols <- c(LUcols, 'black')}
@@ -141,3 +148,49 @@ saveVideo(
   video.name = paste0("Capitals_",scenario,".mp4"))
 
 
+
+#Next, vector maps
+
+cDat <- readr::read_csv(paste0("Data/",scenario,"/",runID,"/CRAFTYmunisLC.csv"))
+
+#note following shp was created using simplyfying_shapefiles.r
+BRmunis <- st_read("Data/Vector/BRmunis_sim10_simple2.shp")
+
+yr <- 2001
+
+for(yr in plot_yrs){
+
+  cDat_map <- left_join(BRmunis, filter(cDat, Year == yr), by = c("CD_GEOCMUn" ="muniID")) 
+  outline <- st_union(st_buffer(cDat_map, 0))
+  
+  #create land cover palette
+  map_pal <- c("darkgreen", "darkcyan", "green", "grey", "khaki")
+
+  #plot observed vs modelled modal muni land cover
+  plot(cDat_map["ObsMode"], pal = map_pal, graticule = st_crs(cDat_map), axes = TRUE, lty = 0, main = paste(yr,"Observed Mode LC"), key.pos = NULL)
+  plot(outline, fill = NULL, add = T)
+  legend("bottomright", cex = 1.3, c("Nature", "Other Agri", "Agriculture", "Other", "Pasture"), fill = map_pal)
+  
+  plot(cDat_map["ModMode"], pal = map_pal, graticule = st_crs(cDat_map), axes = TRUE, lty = 0, main = paste(yr,"Modelled Mode LC"), key.pos = NULL)
+  plot(outline, fill = NULL, add = T)
+  legend("bottomright", cex = 1.3, c("Nature", "Other Agri", "Agriculture", "Other", "Pasture"), fill = map_pal)
+}
+
+map_pal <- c("forestgreen", "darkcyan", "wheat2", "black", "orange2")
+
+map_labs <- c("Nature", "Other Agri", "Agriculture", "Other", "Pasture")
+
+om <- ggplot(cDat_map) +
+  geom_sf(aes(fill=as.factor(ObsMode)), color = NA) +
+  scale_fill_manual(values=map_pal, labels=map_labs, name="Land Cover") +
+  theme(legend.position = c(0.825, 0.2)) + 
+  ggtitle(paste0(yr," Observed Mode LC"))
+  
+mm <- ggplot(cDat_map) +
+  geom_sf(aes(fill=as.factor(ModMode)), color = NA) +
+  scale_fill_manual(values=map_pal, labels=map_labs, name="Land Cover") +
+  theme(legend.position = c(0.825, 0.2)) + 
+  ggtitle(paste0(yr," Modelled Mode LC"))  
+
+
+ggarrange(om, mm, nrow = 1, ncol = 2, common.legend=T, legend="bottom")
