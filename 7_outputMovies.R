225a77f050a7d9c5376608f9fcbd2d38efd00d1a
jamesdamillington@gmail.com <jamesdamillington@gmail.com>
2018-07-23 12:49:26 +0100

    moved observed map creation code to outputMovies script (difficult to import and combined png).

--- 7_outputMovies.R
+++ 7_outputMovies.R
@@ -23,6 +23,8 @@ library(rasterVis)
 library(gridExtra)
 library(animation)
 library(sf)
+#library(png)
+#library(grid)
 
 library(viridisLite)
 
@@ -39,31 +41,8 @@ outputRaster <- function(data, variable){
 }
 
 
-sim_yrs <- seq(2000, 2015, 1)   #movie made for all these years
-fig_yrs <- c(2000, 2005, 2010, 2015) #figures output for only these years 
-
-#set for the run in CRAFTY (althrough runID difficult to control)
-scenario <- "Testing"
-runID <- "0-0"
-
-
-#First, Raster maps
-mps <- list()
-lus <- list()
-
-for(i in seq_along(sim_yrs)){
-  
-  #Load model output data
-  output <- read_csv(paste0("Data/",scenario,"/",runID,"/",scenario,"-",runID,"-Cell-",sim_yrs[i],".csv"))
-  
-  LU <- outputRaster(output, "LandUseIndex")
-  Agri <- outputRaster(output, "Capital:Agriculture")
-  Nat <- outputRaster(output, "Capital:Nature")
-  Infra <- outputRaster(output, "Capital:Infrastructure")
-  OAg <- outputRaster(output, "Capital:Other Agriculture")
-  Aces <- outputRaster(output, "Capital:Acessibility")
-  
-  pl <- list()  #this will hold the plots for ths year
+#LU is a raster object
+makeModLUmap <- function(LU, year) {
   
   #add categories for later plotting etc. (see https://stackoverflow.com/a/37214431)
   LU <- ratify(LU)     #tell R that the map raster is categorical 
@@ -81,7 +60,7 @@ for(i in seq_along(sim_yrs)){
     labs <- c(labs, "Maize") 
     LUcols <- c(LUcols, 'wheat2')}
   if(2 %in% uLU) { 
-    labs <- c(labs, "Double Cropping") 
+    labs <- c(labs, "Dbl Crp") 
     LUcols <- c(LUcols, 'wheat3')}
   if(3 %in% uLU) { 
     labs <- c(labs, "Second N") 
@@ -102,9 +81,91 @@ for(i in seq_along(sim_yrs)){
   rat$LandUse <- labs  
   levels(LU) <- rat 
 
-  #create the LU plot and add to the list
-  pl[[1]] <- levelplot(LU, att = "LandUse", col.regions=LUcols, main = "Modelled Land Use")  
-  LUmap <- levelplot(LU, att = "LandUse", col.regions=LUcols, main = paste0("Modelled Land Use ",sim_yrs[i]))  
+  LUmap <- levelplot(LU, att = "LandUse", col.regions=LUcols, main = paste0("Modelled Land Use ",year))  
+
+  return(LUmap)
+  
+}
+
+makeObsLUmap <- function(LU, year) {
+  
+  maskmap <- raster(paste0("Data/ObservedLCmaps/sim10_BRmunis_latlon_5km_2018-04-27.asc"))
+  
+  LU <- mask(LU, maskmap)
+  LU <- trim(LU)
+  
+  #add categories for later plotting etc. (see https://stackoverflow.com/a/37214431)
+  LU <- ratify(LU)     #tell R that the map raster is categorical 
+  rat <- levels(LU)[[1]]    #apply the levels (i.e. categories) 
+
+  uLU <- unique(LU) 
+
+  LUcols <- c()
+  LUlabs <- c()
+  
+  if(1 %in% uLU) { 
+    LUcols <- c(LUcols, 'forestgreen') 
+    LUlabs <- c(LUlabs, 'Nature')  }
+  if(2 %in% uLU) { 
+    LUcols <- c(LUcols, 'darkcyan') 
+    LUlabs <- c(LUlabs, 'Other Agri') }
+  if(3 %in% uLU) { 
+    LUcols <- c(LUcols, 'wheat2') 
+    LUlabs <- c(LUlabs, 'Agriculture') }
+  if(4 %in% uLU) { 
+    LUcols <- c(LUcols, 'black') 
+    LUlabs <- c(LUlabs, 'Other') }
+  if(5 %in% uLU) { 
+    LUcols <- c(LUcols, 'orange2') 
+    LUlabs <- c(LUlabs, 'Pasture') }
+  
+  rat$LandUse <- LUlabs  
+  levels(LU) <- rat 
+  
+  p <- levelplot(LU, att = "LandUse", col.regions=LUcols, main = paste0("Observed Land Use ",year))  
+
+  return(p)
+}
+
+
+
+sim_yrs <- seq(2000, 2005, 1)   #movie made for all these years
+fig_yrs <- c(2000, 2001, 2010, 2015) #figures output for only these years 
+
+#set for the run in CRAFTY (althrough runID difficult to control)
+scenario <- "Testing"
+runID <- "0-0"
+cl <- "PastureB"  #classification for observed LU map
+
+#First, Raster maps
+mps <- list()
+lus <- list()
+
+for(i in seq_along(sim_yrs)){
+  
+  #Load model output data
+  output <- read_csv(paste0("Data/",scenario,"/",runID,"/",scenario,"-",runID,"-Cell-",sim_yrs[i],".csv"))
+  
+  LU <- outputRaster(output, "LandUseIndex")
+  Agri <- outputRaster(output, "Capital:Agriculture")
+  Nat <- outputRaster(output, "Capital:Nature")
+  Infra <- outputRaster(output, "Capital:Infrastructure")
+  OAg <- outputRaster(output, "Capital:Other Agriculture")
+  Aces <- outputRaster(output, "Capital:Acessibility")
+  
+  pl <- list()  #this will hold the plots for the all map for this year
+  lul <- list()  #this will hold the plots for the LU map for this year
+  
+  #create the Modelled LU plot and add to the list
+  ModLUmap <- makeModLUmap(LU, sim_yrs[i])
+  pl[[1]] <- ModLUmap
+  
+  
+  ObsLU <- raster(paste0("Data/ObservedLCmaps/brazillc_",sim_yrs[i],"_",cl,".asc"))
+
+  ObsLUmap <- makeObsLUmap(ObsLU, sim_yrs[i])
+  lul[[1]] <- ObsLUmap
+  lul[[2]] <- ModLUmap
   
   #now create Capital maps (all with same palette)
   #ras_pal <- colorRampPalette(brewer.pal(9,"YlOrBr"))(100)
@@ -124,17 +185,19 @@ for(i in seq_along(sim_yrs)){
     main = (rl_names[j]))
   
     #add it to the list
-    pl[[j+1]] <- p    #+1 because LU is in furst slot
+    pl[[j+1]] <- p    #+1 because LU is in first slot
   }
   
   mps[[i]] <- marrangeGrob(pl, nrow = 3, ncol = 2, top = paste0(sim_yrs[i]))
-  lus[[i]] <- LUmap
+  lus[[i]] <- marrangeGrob(lul, nrow = 1, ncol = 2, top = paste0(sim_yrs[i]))
+  
+  #lus[[i]] <- LUmap
   
   #if we want this year saved as an image 
   if(sim_yrs[i] %in% fig_yrs) {
   ggsave(paste0("Data/",scenario,"/",runID,"/RasterOutput_AllMaps",sim_yrs[i],".png"), plot = mps[[i]], width=25, height=25, units="cm", dpi = 200)
 
-  ggsave(paste0("Data/",scenario,"/",runID,"/RasterOutput_LUMap",sim_yrs[i],".png"), plot = arrangeGrob(lus[[i]]), width=15, height=15, units="cm", dpi = 300)
+  ggsave(paste0("Data/",scenario,"/",runID,"/RasterOutput_LUMap",sim_yrs[i],".png"), plot = lus[[i]], width=20, height=12.5, units="cm", dpi = 300)
   }
     
 }
