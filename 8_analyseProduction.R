7cf9657702c5b3069e94aa0d6edb988fb106f058
jamesdamillington@gmail.com <jamesdamillington@gmail.com>
2018-07-27 19:49:33 +0100

    added plot functionality

--- 8_analyseProduction.R
+++ 8_analyseProduction.R
@@ -4,11 +4,17 @@
 rm(list=ls())
 
 library(tidyverse)
+library(ggplot2)
 
 #set for the run in CRAFTY (althrough runID difficult to control)
 scenario <- "Testing_2018-07-24"
 runID <- "0-0"
 sim_yrs <- seq(2000, 2015, 1)   #consolidate these years
+
+#output can be printed to pdf by setting following variable appropriately (TRUE/FALSE)
+pdfprint <- TRUE
+output_name <- paste0("Data/",scenario,"/",runID,"/ProductionAnalysis.pdf")
+
 
 #empty table to populate from files below
 all_dat <- data.frame(
@@ -18,7 +24,6 @@ all_dat <- data.frame(
     value_gg = numeric()
     
   )
-  
 tbl_df(all_dat)
 
 #loop through all files 
@@ -41,3 +46,48 @@ for(i in seq_along(sim_yrs)){
     add_row(commodity = "Dairy", measure = "Export", year = sim_yrs[i], value_gg = as.numeric(dat[34,2]))
 
 }
+
+if(pdfprint) {
+  pdf(file = output_name)
+}
+
+#now plot
+#timelines of production, storage, export by commodity
+all_dat %>% 
+  filter(commodity == "Soy") %>%
+  ggplot(aes(x=year, y=value_gg, group=measure)) +
+  geom_line(aes(color=measure)) +
+  ylab("Value (gg)") +
+  xlab("Year") +
+  ggtitle("Soy") 
+
+all_dat %>% 
+  filter(commodity == "Maize") %>%
+  ggplot(aes(x=year, y=value_gg, group=measure)) +
+  geom_line(aes(color=measure)) +
+  ylab("Value (gg)") +
+  xlab("Year") +
+  ggtitle("Maize")
+
+all_dat %>% 
+  filter(commodity == "Meat") %>%
+  ggplot(aes(x=year, y=value_gg, group=measure)) +
+  geom_line(aes(color=measure)) +
+  ylab("Value (gg)") +
+  xlab("Year") +
+  ggtitle("Meat")
+
+all_dat %>% 
+  filter(commodity == "Dairy") %>%
+  ggplot(aes(x=year, y=value_gg, group=measure)) +
+  geom_line(aes(color=measure)) +
+  ylab("Value (gg)") +
+  xlab("Year") +
+  ggtitle("Dairy")
+
+
+if(pdfprint) {
+  dev.off()
+}
+
+#how to get and plot demand from stella?
