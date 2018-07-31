49663d6ebda1e0a697f5b03e8a9402129defb6c5
jamesdamillington@gmail.com <jamesdamillington@gmail.com>
2018-07-31 18:06:02 +0100

    i) corrected read_csv input types, ii) remove outline, iii) added scenario to filename

--- 4_LCcalibrationMaps_5LCs.r
+++ 4_LCcalibrationMaps_5LCs.r
@@ -16,16 +16,17 @@ library(sf)
 library(RColorBrewer)  #for plotting
 
 #this directory should exist and contain the CRAFTYmunisServCap.csv
-run_name <- "test"
+scenario <- "Testing_2018-07-31"
+runID <- "0-0"
 
 #input/putput variables
-output_name <- paste0("Data/",run_name,"/LCcomparisonMaps.pdf")
+output_name <- paste0("Data/",scenario,"/",runID,"/",scenario,"_LCcomparisonMaps.pdf")
 
-cDat <- readr::read_csv(paste0("Data/",run_name,"/CRAFTYmunisLC.csv"))
+cDat <- readr::read_csv(paste0("Data/",scenario,"/",runID,"/",scenario,"_CRAFTYmunisLC.csv"),
+  col_types = cols(Year = col_integer(), diffcProp3 = col_double()))  #needed to ensure correct import (many zeros in diffcProp3 at top of file)
 
 #note following shp was created using simplyfying_shapefiles.r
 BRmunis <- st_read("Data/Vector/BRmunis_sim10_simple2.shp")
-
 
 if(pdfplot) {
   pdf(file = output_name)
@@ -36,24 +37,20 @@ if(pdfplot) {
 for(yr in calib_yrs){
 
   cDat_map <- left_join(BRmunis, filter(cDat, Year == yr), by = c("CD_GEOCMUn" ="muniID")) 
-  outline <- st_union(st_buffer(cDat_map, 0))
-  
+
   #create land cover palette
   map_pal <- c("darkgreen", "darkcyan", "green", "grey", "khaki")
 
   #plot observed vs modelled modal muni land cover
   plot(cDat_map["ObsMode"], pal = map_pal, graticule = st_crs(cDat_map), axes = TRUE, lty = 0, main = paste(yr,"Observed Mode LC"), key.pos = NULL)
-  plot(outline, fill = NULL, add = T)
   legend("bottomright", cex = 1.3, c("Nature", "Other Agri", "Agriculture", "Other", "Pasture"), fill = map_pal)
   
   plot(cDat_map["ModMode"], pal = map_pal, graticule = st_crs(cDat_map), axes = TRUE, lty = 0, main = paste(yr,"Modelled Mode LC"), key.pos = NULL)
-  plot(outline, fill = NULL, add = T)
   legend("bottomright", cex = 1.3, c("Nature", "Other Agri", "Agriculture", "Other", "Pasture"), fill = map_pal)
 
   
   #map of muni mode correct/incorrect   
   plot(cDat_map["diffcMode"], pal = c("darkgreen","red"), graticule = st_crs(cDat_map), axes = TRUE, lty = 0, main = paste(yr,"Model vs Obs Mode Comparison"), key.pos = NULL)  
-  plot(outline, fill = NULL, add = T)
   legend("bottomright", cex = 1.3, c("Correct", "Incorrect"), fill = c("darkgreen","red"))
   
   
@@ -65,32 +62,20 @@ for(yr in calib_yrs){
 
   #total count of cells incorrect
   plot(cDat_map["cellDiffcCount"], pal = cell_pal, breaks = seq(0,errorMax, length.out = length(cell_pal)+1), graticule = st_crs(cDat_map), axes = TRUE, lty = 0, main = paste(yr,"Count Incorrect Cells"))
-  plot(outline, fill = NULL, add = T)
 
   #proportion of cells incorrect 
   plot(cDat_map["cellDiffcProp"], pal = cell_pal, breaks = seq(0,1, length.out = length(cell_pal)+1), graticule = st_crs(cDat_map), axes = TRUE, lty = 0, main = paste(yr,"Prop Incorrect Cells"))
-  plot(outline, fill = NULL, add = T)
-  
+
   
   #for LC proportion accuracy maps
   prop_pal <- brewer.pal(11, "RdYlGn")
   
   #difference in proportion predictions (negative is under-prediction by model, positive over-prediction)
   plot(cDat_map["diffcProp1"], pal = prop_pal, breaks = seq(-1,1, length.out = length(prop_pal)+1), graticule = st_crs(cDat_map), axes = TRUE, lty = 0, main = paste(yr,"Nature Prop Diffc"))
-  plot(outline, fill = NULL, add = T)
-  
   plot(cDat_map["diffcProp2"], pal = prop_pal, breaks = seq(-1,1, length.out = length(prop_pal)+1), graticule = st_crs(cDat_map), axes = TRUE, lty = 0, main = paste(yr,"Other Agri Prop Diffc"))
-  plot(outline, fill = NULL, add = T)
-  
   plot(cDat_map["diffcProp3"], pal = prop_pal, breaks = seq(-1,1, length.out = length(prop_pal)+1), graticule = st_crs(cDat_map), axes = TRUE, lty = 0, main = paste(yr,"Agriculture Prop Diffc"))
-  plot(outline, fill = NULL, add = T)
-  
   plot(cDat_map["diffcProp5"], pal = prop_pal, breaks = seq(-1,1, length.out = length(prop_pal)+1), graticule = st_crs(cDat_map), axes = TRUE, lty = 0, main = paste(yr,"Pasture Prop Diffc"))
-  plot(outline, fill = NULL, add = T)
-  
   plot(cDat_map["diffcProp4"], pal = prop_pal, breaks = seq(-1,1, length.out = length(prop_pal)+1), graticule = st_crs(cDat_map), axes = TRUE, lty = 0, main = paste(yr,"Other Prop Diffc"))
-  plot(outline, fill = NULL, add = T)
-  
   par(mfrow=c(1,1))  #needed to ensure plotting plays nicely with key.pos = NULL above
   
 }
--- A_allOutputAnalysis.r
+++ A_allOutputAnalysis.r
@@ -584,7 +584,7 @@ BRmunis <- st_read("Data/Vector/BRmunis_sim10_simple2.shp")
 
 
 if(pdfprint) {
-  pdf(file = paste0(data_dir,"LCcomparisonMaps.pdf"))
+  pdf(file = paste0(data_dir,scenario,"_LCcomparisonMaps.pdf"))
 }
 
 ## Maps
@@ -592,24 +592,20 @@ if(pdfprint) {
 for(yr in calib_yrs){
 
   cDat_map <- left_join(BRmunis, filter(cDat, Year == yr), by = c("CD_GEOCMUn" ="muniID")) 
-  outline <- st_union(st_buffer(cDat_map, 0))
-  
+
   #create land cover palette
   map_pal <- c("darkgreen", "darkcyan", "green", "grey", "khaki")
 
   #plot observed vs modelled modal muni land cover
   plot(cDat_map["ObsMode"], pal = map_pal, graticule = st_crs(cDat_map), axes = TRUE, lty = 0, main = paste(yr,"Observed Mode LC"), key.pos = NULL)
-  plot(outline, fill = NULL, add = T)
   legend("bottomright", cex = 1.3, c("Nature", "Other Agri", "Agriculture", "Other", "Pasture"), fill = map_pal)
   
   plot(cDat_map["ModMode"], pal = map_pal, graticule = st_crs(cDat_map), axes = TRUE, lty = 0, main = paste(yr,"Modelled Mode LC"), key.pos = NULL)
-  plot(outline, fill = NULL, add = T)
   legend("bottomright", cex = 1.3, c("Nature", "Other Agri", "Agriculture", "Other", "Pasture"), fill = map_pal)
 
   
   #map of muni mode correct/incorrect   
   plot(cDat_map["diffcMode"], pal = c("darkgreen","red"), graticule = st_crs(cDat_map), axes = TRUE, lty = 0, main = paste(yr,"Model vs Obs Mode Comparison"), key.pos = NULL)  
-  plot(outline, fill = NULL, add = T)
   legend("bottomright", cex = 1.3, c("Correct", "Incorrect"), fill = c("darkgreen","red"))
   
   
@@ -621,32 +617,20 @@ for(yr in calib_yrs){
 
   #total count of cells incorrect
   plot(cDat_map["cellDiffcCount"], pal = cell_pal, breaks = seq(0,errorMax, length.out = length(cell_pal)+1), graticule = st_crs(cDat_map), axes = TRUE, lty = 0, main = paste(yr,"Count Incorrect Cells"))
-  plot(outline, fill = NULL, add = T)
 
   #proportion of cells incorrect 
   plot(cDat_map["cellDiffcProp"], pal = cell_pal, breaks = seq(0,1, length.out = length(cell_pal)+1), graticule = st_crs(cDat_map), axes = TRUE, lty = 0, main = paste(yr,"Prop Incorrect Cells"))
-  plot(outline, fill = NULL, add = T)
-  
+
   
   #for LC proportion accuracy maps
   prop_pal <- brewer.pal(11, "RdYlGn")
   
   #difference in proportion predictions (negative is under-prediction by model, positive over-prediction)
   plot(cDat_map["diffcProp1"], pal = prop_pal, breaks = seq(-1,1, length.out = length(prop_pal)+1), graticule = st_crs(cDat_map), axes = TRUE, lty = 0, main = paste(yr,"Nature Prop Diffc"))
-  plot(outline, fill = NULL, add = T)
-  
   plot(cDat_map["diffcProp2"], pal = prop_pal, breaks = seq(-1,1, length.out = length(prop_pal)+1), graticule = st_crs(cDat_map), axes = TRUE, lty = 0, main = paste(yr,"Other Agri Prop Diffc"))
-  plot(outline, fill = NULL, add = T)
-  
   plot(cDat_map["diffcProp3"], pal = prop_pal, breaks = seq(-1,1, length.out = length(prop_pal)+1), graticule = st_crs(cDat_map), axes = TRUE, lty = 0, main = paste(yr,"Agriculture Prop Diffc"))
-  plot(outline, fill = NULL, add = T)
-  
   plot(cDat_map["diffcProp5"], pal = prop_pal, breaks = seq(-1,1, length.out = length(prop_pal)+1), graticule = st_crs(cDat_map), axes = TRUE, lty = 0, main = paste(yr,"Pasture Prop Diffc"))
-  plot(outline, fill = NULL, add = T)
-  
   plot(cDat_map["diffcProp4"], pal = prop_pal, breaks = seq(-1,1, length.out = length(prop_pal)+1), graticule = st_crs(cDat_map), axes = TRUE, lty = 0, main = paste(yr,"Other Prop Diffc"))
-  plot(outline, fill = NULL, add = T)
-  
   par(mfrow=c(1,1))  #needed to ensure plotting plays nicely with key.pos = NULL above
   
 }
